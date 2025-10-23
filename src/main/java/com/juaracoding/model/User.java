e575918b3a4eacd31bdb13c9784c2bd52a9551ce
sourchib <muchammadmuchib@gmail.com>
2025-10-23 19:09:36 +0700

    koswara trainer spriboot.io

---
+++ src/main/java/com/juaracoding/model/User.java
@@ -0,0 +1,14 @@
+package com.juaracoding.model;
+
+public class User {
+}
+
+/*
+IntelliJ IDEA 2022.3.1 (Community Edition)
+Build #IC-223.8214.52, built on December 20, 2022
+@Author LENOVO a.k.a. M Muchib Zainul Fikry
+Java Developer
+Created on 23/10/2025 19:07
+@Last Modified 23/10/2025 19:07
+Version 1.0
+*/
